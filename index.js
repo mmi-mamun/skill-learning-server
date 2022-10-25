5a6986a0b15df112458d9f2098402bd4af13056c
mmi-mamun <muzahidulislam735@gmail.com>
2022-10-25 15:00:05 +0600

    install cors for  give permission to access data from other server or website

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+const cors = require('cors');
+app.use(cors());
+
 app.get('/', (req, res) => {
     res.send(`*ASSALAMU ALAIKUM* 
     ____ SKILL-LEARNING-SERVER-SITE is running...`)
